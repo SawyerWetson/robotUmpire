91715aa87694b62e2deb2b904c648639d8241192
TheMagicHatCoder <sawyer.wetson@gmail.com>
2025-06-25 12:32:07 -0500

    Create notice:isacdxu_delete_please_notice_from_SawyerWetson.rs

---
+++ notice:isacdxu_delete_please_notice_from_SawyerWetson.rs
@@ -0,0 +1,3 @@
+fn main() {
+  println!("@isacdxu, please do not take my work in this repository that i've given you, you may fill out the README, but please credit me for the code that i've given you, you don't have to credit me, but it would be the nice thing to do");
+} 
